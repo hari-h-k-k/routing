029e28fb87065853749d4aeb2f76bbe61552e5be
Harikrishnan.K <harikdas2001@gmail.com>
2021-06-19 19:54:17 +0530

    linked

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 import About from "./pages/about";
+import NavBar from './Navbar'
 import {Route,Link} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Route exact path ='/' component={Home}/>
       <Route exact path ='/contact' component={Contact}/>
       <Route exact path ='/about' component={About}/>
