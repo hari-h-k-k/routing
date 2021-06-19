2a0dcc448881e93a2e4dbf2c90f00aae7d0ea223
Harikrishnan.K <harikdas2001@gmail.com>
2021-06-19 19:56:10 +0530

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,6 +4,9 @@ import {Link} from 'react-router-dom';
 function NavBar(){
     return(<ul>
         <li>
+            <Link to='/'>Home</Link>
+        </li>
+        <li>
             <Link to='/contact'>Contact</Link>
         </li>
         <li>
